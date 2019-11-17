d7fd9810c4e2ef46bfe78e7f0489b99a85f29f69
haoyu9112 <haoyu9112@gmail.com>
2019-11-16 22:41:58 -0800

    [core] add test account

--- config/config.py
+++ config/config.py
@@ -9,18 +9,14 @@ class TradingConfig(object):
         self.dir_path = os.path.dirname(os.path.realpath(__file__))
         self._load_config(config)
 
-    def _load_config(self, config="development"):
+    def _load_config(self, env="development"):
         # load config
-        yaml_file = "{}/{}.yaml".format(self.dir_path, config)
+        yaml_file = "{}/{}.yaml".format(self.dir_path, env)
         priv_yaml_file = "{}/priv.yaml".format(self.dir_path)
 
         print("load config: {}".format(yaml_file))
         with open(yaml_file, 'r') as f:
             self.config = yaml.safe_load(f)
-
-        print("load priv config: {}".format(yaml_file))
-        with open(priv_yaml_file, 'r') as f:
-            self.priv_config = yaml.safe_load(f)
 
         self.logging_level = self.config["dev"]["logging-level"]
         self.ib_ip = self.config["ib"]["ip"]
@@ -32,14 +28,23 @@ class TradingConfig(object):
         assert self.ib_port
 
         self.alpaca_url = self.config["alpaca"]["url"]
-        self.alpaca_id = self.priv_config["alpaca"]["id"]
-        self.alpaca_key = self.priv_config["alpaca"]["key"]
-
         assert self.alpaca_url
-        assert self.alpaca_id   # it's in priv.yaml
-        assert self.alpaca_key  # it's in priv.yaml
 
         # data
         self.csv_folder = self.config["data"]["csv_folder"]
 
         assert self.csv_folder
+
+        # private data
+        if env != "test":
+            print("load priv config: {}".format(yaml_file))
+            with open(priv_yaml_file, 'r') as f:
+                self.priv_config = yaml.safe_load(f)
+
+            self.alpaca_id = self.priv_config["alpaca"]["id"]
+            self.alpaca_key = self.priv_config["alpaca"]["key"]
+        else:
+            self.alpaca_id = self.config["alpaca"]["id"]
+            self.alpaca_key = self.config["alpaca"]["key"]
+        assert self.alpaca_id  # it's in priv.yaml
+        assert self.alpaca_key  # it's in priv.yaml
